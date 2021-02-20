0662a525de2e94bec39daa0dba262b117cd22972
Mizael A. Silva Moraes <mysael90@gmail.com>
2021-02-20 19:58:13 -0300

    .

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const velocity = 10
-
-if (velocity >= 60) {
-    console.log('Multado')
-}
-
-else {
-    console.log('Ok')
-    
+function calc() {
+    var resultado = document.querySelector('#result')
+    var textImput = document.querySelector('#velocidade')
+    var velocidade = (textImput.value)
+    if (velocidade > 80) {
+        resultado.innerHTML = `Sua velocidade é ${velocidade}  você foi multado`
+    }
 }
