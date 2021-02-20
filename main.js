b23aa70df65846b19aaeb76ff37efd3c35a9049d
Mizael A. Silva Moraes <mysael90@gmail.com>
2021-02-20 20:59:43 -0300

    Update main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function ok() {
-    var resultado = document.querySelector('#res')
-    var nascionalidade = document.querySelector('#pais')
-    if (nascionalidade != 'Brasil') {
-        res.innerHTML = `Seja bem vindo estrangeiro`
-    } else {
-        res.innerHTML = `Patria amada ${nascionalidade}`
-    }
-}
